3733fd493095e4ff0d32ff99479c5978b5c0310c
David Hahn <david@40digits.com>
2016-02-18 14:17:19 -0600

    Add craft

---
+++ craft/config/db.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Database Configuration
+ *
+ * All of your system's database configuration settings go in here.
+ * You can see a list of the default settings in craft/app/etc/config/defaults/db.php
+ */
+
+$customConfig = array(
+
+	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+	'server' => CRAFT_DB_SERVER,
+
+	// The name of the database to select.
+	'database' => CRAFT_DB_NAME,
+
+	// The database username to connect with.
+	'user' => CRAFT_DB_USER,
+
+	// The database password to connect with.
+	'password' => CRAFT_DB_PASSWORD,
+
+	// The prefix to use when naming tables. This can be no more than 5 characters.
+	'tablePrefix' => 'craft',
+
+);
+
+// If a local config file exists, merge any local config settings
+if (is_array($customLocalDBConfig = @include(CRAFT_CONFIG_PATH . 'local/db.php')))
+{
+  $customConfig = array_merge($customConfig, $customLocalDBConfig);
+}
+
+return $customConfig;
---
+++ craft/config/local/db.example.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Local DB Config Override
+ *
+ * Overrides added here will get will override config/db.php
+ */
+
+return array(
+
+  // The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+  'server' => '127.0.0.1',
+
+  // The name of the database to select.
+  'database' => 'craft-boilerplate',
+
+  // The database username to connect with.
+  'user' => 'root',
+
+  // The database password to connect with.
+  'password' => '',
+
+  // The prefix to use when naming tables. This can be no more than 5 characters.
+  'tablePrefix' => 'craft',
+
+);
